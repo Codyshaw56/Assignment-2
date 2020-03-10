adbe0f2ec12321bce571d94674fa7f97f3a626e3
Cody Shaw <Codyshaw56@hotmail.com>
2020-03-10 12:58:04 +0000

    yyyyy

--- volumes/Form1.cs
+++ volumes/Form1.cs
@@ -39,15 +39,108 @@ namespace volumes
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            double RS1 = double.Parse(textBox1.Text);
-            double RS2 = double.Parse(textBox2.Text);
-            double RP1 = double.Parse(textBox3.Text);
-            double RP2 = double.Parse(textBox4.Text);
-            double RS1 = double.Parse(textBox5.Text);
-            double RS2 = double.Parse(textBox6.Text);
-            double RS3 = double.Parse(textBox7.Text);
-            double RP1 = double.Parse(textBox8.Text);
-            double RP2 = double.Parse(textBox9.Text);
-            double RP3 = double.Parse(textBox10.Text);
+
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double RS1, RS2;
+            try
+            {
+                RS1 = double.Parse(textBox1.Text);
+                RS2 = double.Parse(textBox2.Text);
+                //reading text from textboxes
+            }
+            catch
+            {
+                MessageBox.Show("stop right there bromigo");
+                RS1 = 0.0;
+                RS2 = 0.0;
+                //catching errors
+            }
+
+            double RT1 = RS1 + RS2;
+            //calculate series resistance
+            textBox14.AppendText(RT1.ToString());
+            //show solution in text box
+
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            double RP1, RP2;
+            try
+            {
+                RP1 = double.Parse(textBox3.Text);
+                RP2 = double.Parse(textBox4.Text);
+            }
+            catch
+            {
+                MessageBox.Show("stop right there bromigo");
+                RP1 = 0.0;
+                RP2 = 0.0;
+            }
+
+            double RT2 = 1.0 / ((1.0 / RP1) + (1.0 / RP2));
+            textBox15.AppendText(RT2.ToString());
+
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            double RS1, RS2, RS3;
+            try
+            {
+                RS1 = double.Parse(textBox5.Text);
+                RS2 = double.Parse(textBox6.Text);
+                RS3 = double.Parse(textBox7.Text);
+                
+            }
+            catch
+            {
+                MessageBox.Show("stop right there bromigo");
+                RS1 = 0.0;
+                RS2 = 0.0;
+                RS3 = 0.0;
+            }
+
+            double RT3 = RS1 + RS2 + RS3;
+            textBox16.AppendText(RT3.ToString());
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            double RP1, RP2, RP3;
+            try
+            {
+                RP1 = double.Parse(textBox8.Text);
+                RP2 = double.Parse(textBox9.Text);
+                RP3 = double.Parse(textBox10.Text);
+            }
+            catch
+            {
+                MessageBox.Show("stop right there bromigo");
+                RP1 = 0.0;
+                RP2 = 0.0;
+                RP3 = 0.0;
+            }
+
+            double RT4 = 1.0 / ((1.0 / RP1) + (1.0 / RP2) + (1.0 / RP3));
+            textBox17.AppendText(RT4.ToString());
+           
+            // i started with the series because addition was in my case much easier to code and 
+        }
+    }
 }
+
+    
+      
+
+
+    
+
+
+
+
